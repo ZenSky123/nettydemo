36491ba0658a83969412f1fa4aaa41feea864a8e
zensky123 <zensky@126.com>
2021-05-31 14:15:09 +0800

    Initial commit

---
+++ src/main/java/simpleEchoServer/Server.java
@@ -0,0 +1,40 @@
+package simpleEchoServer;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class Server {
+    private final int port = 5000;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Server().start();
+    }
+
+    public void start() throws InterruptedException {
+        final ServerHandler serverHandler = new ServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(serverHandler);
+                        }
+                    });
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+}
